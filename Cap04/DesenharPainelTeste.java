8c3d44189192337aa74b61d2b0824379e6b1e548
Pedro Arns Junior <69806103+pedrojunior079@users.noreply.github.com>
2021-09-21 20:43:09 -0300

    update capitulo 4

---
+++ Cap04/DesenharPainelTeste.java
@@ -0,0 +1,22 @@
+//Figura 4.19: DesenharPainelTeste.java
+//Criando JFrame para exibir um DrawPanel
+import javax.swing.JFrame;
+
+public class DesenharPainelTeste
+{
+	public static void mmain(String[] args)
+	{
+		//cria um ṕainel que contem nosso desenho
+		
+		//cria um painel que contém nosso desenho
+		DesenharPainel painel = new DesenharPainel();
+			
+		//cria um novo quadro para armazenar o painel
+		JFrame aplicacao = new JFrame();
+			
+		//configura o frame para ser encerrado quando ele é fechado
+		aplicacao.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		aplicacao.add(painel);//adiciona o painel ao frame
+		aplicacao.setVisible(true);//torna o frame visivel
+	}
+}//fim da classe DesenharPainelTeste
