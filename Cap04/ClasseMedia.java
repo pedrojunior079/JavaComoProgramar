8638998f35e8d26d9bd62eacc98ac50cacdfa8b4
Pedro Arns Junior <69806103+pedrojunior079@users.noreply.github.com>
2021-09-21 00:25:17 -0300

    update cap04

--- Cap04/ClasseMedia.java
+++ Cap04/ClasseMedia.java
@@ -1,11 +1,32 @@
 //Figura 4.8: ClasseMedia.java
 //Resolvendo o problema da média da classe usando repetição controlada por contador
-import java.util.Scanner;
+import java.util.Scanner; //programa utiliza a classe Scanner
 
 public class ClasseMedia 
 {
    public static void main(String[] args)
    {
       //cria um Scanner para obter entrada a partir da janela de comando
+      Scanner input = new Scanner(System.in);
+      
+      //fase de inicialização
+      int total = 0; //inicializa a soma das notas inseridas pelo usuario
+      int notaContador = 1; //iniicializa numero da nota a ser inserido pelo usuario
+
+      //fase de processamento utiliza a soma das notas inseridas pelo usuario
+      while(notaContador <= 10) //faz o loop 10 vezes
+      {
+         System.out.print("Entre nota: "); //prompt
+         int nota = input.nextInt(); //insere a proxima nota
+         total = total + nota; //adiciona nota a total
+         notaContador = notaContador + 1; //incrementa o contador por + 1
+      }
+
+      //fase de término
+      int media = total / 10; //divisão de inteiros produz um resultado inteiro
+
+      //exibe o total e a média das notas
+      System.out.printf("total de todas as 10 notas foi  %d%n", total);
+      System.out.printf("Classe Media foi %d%n", media);
    }    
-}
+}//fim da classe ClasseMedia
---
+++ Cap04/ClasseMedia1.java
@@ -0,0 +1,43 @@
+//Figura 4.10: ClasseMedia1.java
+//Resolvendo o problema da média da classe usando repetição controlada por sentinela
+import java.util.Scanner; //programa utiliza a classe Scanner
+
+public class ClasseMedia1 
+{
+   public static void main(String[] args)
+   {
+      //cria um Scanner para obter entrada a partir da janela de comando
+      Scanner input = new Scanner(System.in);
+      
+      //fase de inicialização
+      int total = 0; //inicializa a soma das notas inseridas pelo usuario
+      int notaContador = 0; //iniicializa numero de notas a ser inseridas até agora
+
+      //fase de processamento 
+      System.out.print("Entre nota ou - 1 para sair: "); 
+      int nota = input.nextInt(); 
+         
+      //faz um loop até ler o valor de sentinela inserido pelo usuario
+      while(nota != -1)
+      {
+         total = total + nota; //adiciona nota a total
+         notaContador = notaContador + 1; //incrementa o contador por + 1
+            
+         //solicita entrada e le a proxima nota fornecida pelo usuario
+         System.out.print("Entre nota ou - 1 para sair: "); 
+         nota = input.nextInt(); 
+      }
+      
+      //fase de término
+      //se usuário inseriu pelo menos uma nota...
+      if(notaContador != 0)
+      {
+         double media = (double) = total/notaContador;
+         //exibe o total e a média das notas
+         System.out.printf("total de todas as 10 notas foi  %d%n", notaContador, total);
+         System.out.printf("Classe Media foi %d%n", media);
+      }
+      else
+         System.out.println("Nenhuma nota foi inserida");
+   }    
+}//fim da classe ClasseMedia1
